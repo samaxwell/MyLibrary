59f740df7290d7e99cf40eb295283f7164da963c
Sean M <sa.maxwell@yahoo.com>
2016-08-20 10:40:17 -0500

    quick fix

--- src/main/java/com/industries/seanimus/Application.java
+++ src/main/java/com/industries/seanimus/Application.java
@@ -1,7 +1,5 @@
 package com.industries.seanimus;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +7,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.industries.seanimus.books.Book;
 import com.industries.seanimus.books.BookDao;
-import com.industries.seanimus.parser.Parser;
-import com.industries.seanimus.people.Person;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
 @Configuration
 @ComponentScan
@@ -22,17 +17,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		
+
 		BookDao jdbcTemplate = new BookDao();
 
-//		List<Book> books = jdbcTemplate.getAllBooks();
-//		for (Book b : books){
-//			System.out.println(b);
-//		}
-		
-//		List<Person> people = jdbcTemplate.getAllPeople();
-//		for (Person p : people){
-//			System.out.println(p);
-//		}
+		// List<Book> books = jdbcTemplate.getAllBooks();
+		// for (Book b : books){
+		// System.out.println(b);
+		// }
+
+		// List<Person> people = jdbcTemplate.getAllPeople();
+		// for (Person p : people){
+		// System.out.println(p);
+		// }
 	}
 }
