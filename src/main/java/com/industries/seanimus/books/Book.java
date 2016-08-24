2028bcf7b7609c61f36abaf71da7ecb3ff35eb79
Sean M <sa.maxwell@yahoo.com>
2016-08-23 22:27:23 -0500

    Add book covers as byte[]

--- src/main/java/com/industries/seanimus/books/Book.java
+++ src/main/java/com/industries/seanimus/books/Book.java
@@ -1,25 +1,36 @@
 package com.industries.seanimus.books;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 
+import javax.imageio.ImageIO;
+
 public class Book {
-	
+
 	private String name;
 	private String authorName;
-	private long isbnNumber;
 	private String category;
+
+	private long isbnNumber;
+
 	private BigDecimal price;
-	
-	public Book(){
+
+	private byte[] cover;
+
+	public Book() {
 	}
 
-	public Book(String name, String authorName, long isbnNumber, String category, BigDecimal price) {
+	public Book(String name, String authorName, long isbnNumber, String category, BigDecimal price, String cover) {
 		super();
 		this.name = name;
 		this.authorName = authorName;
 		this.isbnNumber = isbnNumber;
 		this.category = category;
 		this.price = price;
+		this.setCover(cover);
 	}
 
 	public String getName() {
@@ -42,10 +53,32 @@ public class Book {
 		return price;
 	}
 
+	public byte[] getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover_path) {
+		if (cover_path != null) {
+			File img;
+			try {
+				img = new File(cover_path);
+				BufferedImage bufferedImage = ImageIO.read(img);
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ImageIO.write(bufferedImage, "jpg", baos);
+				this.cover = baos.toByteArray();
+			} catch (IOException e) {
+				System.out.println("THERE WAS AN ERROR CREATING PHOTO FOR " + this.toString());
+			}
+		}
+		else {
+			this.cover = new byte[0];
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "Book [name=" + name + ", category=" + category + ", price=" + price + ", authorName=" + authorName
 				+ ", isbnNumber=" + isbnNumber + "]";
 	}
-	
+
 }
