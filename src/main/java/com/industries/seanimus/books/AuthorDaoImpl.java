9a8a0161878c1ea0abeec178dc3dc0a05fed578c
Sean M <sa.maxwell@yahoo.com>
2016-08-28 14:35:02 -0500

    Minor changes

--- src/main/java/com/industries/seanimus/books/AuthorDaoImpl.java
+++ src/main/java/com/industries/seanimus/books/AuthorDaoImpl.java
@@ -16,14 +16,12 @@ public class AuthorDaoImpl {
 	private JdbcTemplate jdbc;
 
 	public List<Author> getAllAuthors() {
-		// TODO - update querry to view author table
 		return jdbc.query("SELECT * FROM authors", new AuthorRowMapper());
 	}
 
 }
 
 class AuthorRowMapper implements RowMapper<Author> {
-
 	@Override
 	public Author mapRow(ResultSet rs, int arg1) throws SQLException {
 		return new Author(rs.getInt("id"), rs.getString("name"), rs.getDate("dob"), rs.getDate("dod"),
